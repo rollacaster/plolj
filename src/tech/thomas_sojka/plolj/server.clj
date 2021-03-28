437c693c36317838d9ef229fc0fa5e6207d007e3
Thomas Sojka <thsojka@web.de>
2021-03-28 19:12:31 +0200

    Implement volatize on server

--- src/tech/thomas_sojka/plolj/server.clj
+++ src/tech/thomas_sojka/plolj/server.clj
@@ -4,11 +4,58 @@
             [clojure.java.io :as io]
             [clojure.string :as str]
             [compojure.core :refer [defroutes POST]]
+            [mikera.image.core :as img]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.cors :refer [wrap-cors]]
             [ring.middleware.defaults :refer [api-defaults wrap-defaults]]
             [ring.middleware.multipart-params :refer [wrap-multipart-params]]
-            [ring.util.request :refer [body-string]]))
+            [ring.util.request :refer [body-string]]
+            [thi.ng.color.core :as color]
+            [thi.ng.geom.svg.adapter :as adapt]
+            [thi.ng.geom.svg.core :as svg]
+            [thi.ng.math.core :as math]))
+
+(def get-lightness (comp :l color/as-hsla color/int32))
+(def width (Math/floor (/ 1748 3)))
+(def height (Math/floor (/ 1240 3)))
+
+(defn volatize-image
+  ([width height data]
+   (volatize-image width height data {}))
+  ([width height data
+    {:keys [y-line-space dark-ampl light-ampl noise-ampl]
+     :or {y-line-space 7 dark-ampl 2 light-ampl 8 noise-ampl 2}}]
+   (assert y-line-space)
+   (assert dark-ampl)
+   (assert light-ampl)
+   (assert noise-ampl)
+   (for [y (range 0 height y-line-space)]
+     (loop [x 0
+            i 0
+            lines []]
+       (if (< x width)
+         (let [pixel (get data (+ x (* y width)))
+               lightness (get-lightness (or pixel 0))
+               x (Math/min (double width) (Math/floor (+ x (math/map-interval lightness
+                                                                       [0 1]
+                                                                       [dark-ampl light-ampl]))))]
+           (recur x
+                  (inc i)
+                  (conj lines [x (+ y
+                                    y-line-space
+                                    (if (= (mod i 2) 0) (- noise-ampl) noise-ampl))])))
+         lines)))))
+
+(comment
+  (->>(svg/svg
+       {:width width :height height}
+       (->> (img/get-pixels (img/load-image-resource "removed-background/test.jpg"))
+            (volatize-image (.getWidth (img/load-image-resource "removed-background/test.jpg"))
+                            (.getHeight (img/load-image-resource "removed-background/test.jpg")))
+            (map (fn [line] (svg/line-strip line {:stroke "black"})))))
+      (adapt/all-as-svg)
+      (svg/serialize)
+      (spit "test.svg")))
 
 (default-session-options {:strict-host-key-checking :no})
 
