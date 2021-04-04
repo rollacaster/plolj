f5c6d0ba2072c86f7a17ef4f4a2643a129cc9a45
Thomas Sojka <thsojka@web.de>
2021-04-04 18:12:52 +0200

    Move volatize to own route

--- src/tech/thomas_sojka/plolj/components.cljs
+++ src/tech/thomas_sojka/plolj/components.cljs
@@ -5,12 +5,11 @@
    {:style (merge
             {:box-shadow "0 0 #0000, 0 0 #0000, 0 25px 50px -12px rgba(0, 0, 0, 0.25)"
              :margin "auto"
-             :transition "0.5s all"}
+             :transition "0.5s all"
+             :width width
+             :height height}
             style)
      :id "drop_zone"
      :onDragOver (fn [ev] (.preventDefault ev))
      :onDrop on-drop}
-    [:canvas#canvas
-     {:width width
-      :height height}]
    children])
