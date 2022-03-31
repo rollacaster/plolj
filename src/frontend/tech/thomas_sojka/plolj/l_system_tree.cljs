93b525075e39bb3125a84096751941e30c55253f
Thomas Sojka <thsojka@web.de>
2022-03-31 10:35:27 +0200

    L-systems-tree sketch

--- src/frontend/tech/thomas_sojka/plolj/l_system_tree.cljs
+++ src/frontend/tech/thomas_sojka/plolj/l_system_tree.cljs
@@ -1,10 +1,11 @@
 (ns tech.thomas-sojka.plolj.l-system-tree
   (:require
    [tech.thomas-sojka.plolj.components :refer [drawing-canvas]]
-   [tech.thomas-sojka.plolj.constants :refer [cm->pixel]]
    [tech.thomas-sojka.plolj.l-system :as l-system]))
 
-(defn plot-button [svg]
+(def svg (atom nil))
+
+(defn plot-button []
   [:button.shadow-2.pointer.bn.pv2.ph3.br3.grow.outline-0.ml1.mt5.mb1
    {:style {:bottom 0 :background-color "#F3F4F6" }
     :on-click (fn []
@@ -17,46 +18,28 @@
    [:span {:style {:font-size 32 :color "#111827"}} "Plot"]])
 
 (defn main []
-  (let [svg (atom nil)]
-    (fn []
-      [:div
-       (let [width (cm->pixel 29.7)
-             height (cm->pixel 21)]
-         [drawing-canvas
-          {:width width
-           :height height}
-          [:svg.ba
-           {:ref (fn [el] (when el (reset! svg el)))
-            :width width
-            :height height}
-           [:g
-            {:transform "translate(900, 690)"}
-            (l-system/run
-              {:name "tree"
-               :rules [{:from "|", :to "||-(-|+|⬤+|⬤)+(+|-|⬤-|⬤)"}]
-               :axiom "|"
-               :angle (* (/ Math/PI 180) 25)
-               :length 200
-               :radius 3
-               :order 3})]
-           [:g
-            {:transform "translate(500, 690)"}
-            (l-system/run
-              {:name "tree"
-               :rules [{:from "|", :to "||-(-|+|⬤+|⬤)+(+|-|⬤-|⬤)"}]
-               :axiom "|"
-               :angle (* (/ Math/PI 180) 25)
-               :length 200
-               :radius 3
-               :order 2})]
-           [:g
-            {:transform "translate(200, 690)"}
-            (l-system/run
-              {:name "tree"
-               :rules [{:from "|", :to "||-(-|+|⬤+|⬤)+(+|-|⬤-|⬤)"}]
-               :axiom "|"
-               :angle (* (/ Math/PI 180) 25)
-               :length 200
-               :radius 3
-               :order 1})]]])
-       [plot-button {:svg svg}]])))
+  [:div
+   (let [width 300
+         height 300]
+     [drawing-canvas
+      {:width width
+       :height height}
+      [:svg
+       {:ref (fn [el] (when el (reset! svg el)))
+        :width width
+        :height height}
+       [:rect {:x 0 :y 0
+               :width (- width 1) :height (- height 1) :stroke "black"
+               :fill "transparent"
+               :stroke-width 1}]
+       [:g
+        {:transform "translate(170, 295)"}
+        (l-system/run
+          {:name "tree"
+           :rules [{:from "|", :to "||-(-|+|⬤-+|⬤)+(+|-|⬤-|⬤)"}]
+           :axiom "|"
+           :angle (* (/ Math/PI 180) 25)
+           :length 88
+           :radius 3
+           :order 3})]]])
+   [plot-button]])
