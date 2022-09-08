5a8011a376da03f7a7728d1bc0d53bd95f2f4d0c
Thomas Sojka <thsojka@web.de>
2022-09-08 08:58:47 +0200

    [refactor] Use plot-canvas for sierpinski

--- src/frontend/tech/thomas_sojka/plolj/components.cljs
+++ src/frontend/tech/thomas_sojka/plolj/components.cljs
@@ -17,19 +17,19 @@
 
 (defn download-button [{:keys [element]}]
   [:button.shadow-2.pointer.bn.pv2.ph3.br3.grow.outline-0.mb1
-          {:style {:bottom 0 :background-color "#F3F4F6" }
-           :on-click (fn []
-                       (let [el (.createElement js/document "a")]
-                         (.setAttribute el "href" (str "data:text/plain;charset=utf-8," (js/encodeURIComponent (.-outerHTML element))))
-                         (.setAttribute el "download" "sketch.svg")
-                         (set! (.-style.display el) "none")
-                         (.appendChild js/document.body el)
-                         (.click el)
-                         (.removeChild js/document.body el)))}
-          [:span {:style {:font-size 32 :color "#111827"}} "Download"]])
+   {:style {:bottom 0 :background-color "#F3F4F6" }
+    :on-click (fn []
+                (let [el (.createElement js/document "a")]
+                  (.setAttribute el "href" (str "data:text/plain;charset=utf-8," (js/encodeURIComponent (.-outerHTML element))))
+                  (.setAttribute el "download" "sketch.svg")
+                  (set! (.-style.display el) "none")
+                  (.appendChild js/document.body el)
+                  (.click el)
+                  (.removeChild js/document.body el)))}
+   [:span {:style {:font-size 32 :color "#111827"}} "Download"]])
 
 (defn plot-button [{:keys [element]}]
-  [:button.shadow-2.pointer.bn.pv2.ph3.br3.grow.outline-0.ml1.mt5.mb1
+  [:button.shadow-2.pointer.bn.pv2.ph3.br3.grow.outline-0.ml1.mt5.mb1.mr3
    {:style {:bottom 0 :background-color "#F3F4F6" }
     :on-click (fn []
                 (let [s (new js/XMLSerializer)]
@@ -49,6 +49,7 @@
        [drawing-canvas {:width width :height height}
         [:svg
          {:ref (fn [el] (prn el)(when el (reset! svg-node! el)))
+          :xmlns "http://www.w3.org/2000/svg"
           :width width
           :height height}
          [:rect {:x 0 :y 0
@@ -56,4 +57,5 @@
                  :fill "transparent"
                  :stroke-width 1}]
          children]]
-       [plot-button {:element @svg-node!}]])))
+       [plot-button {:element @svg-node!}]
+       [download-button {:element @svg-node!}]])))
--- src/frontend/tech/thomas_sojka/plolj/sierpinski.cljs
+++ src/frontend/tech/thomas_sojka/plolj/sierpinski.cljs
@@ -1,13 +1,9 @@
 (ns tech.thomas-sojka.plolj.sierpinski
-  (:require [reagent.core :as r]
-            [tech.thomas-sojka.plolj.components
-             :refer
-             [download-button drawing-canvas plot-button]]
+  (:require [tech.thomas-sojka.plolj.components :refer [plot-canvas]]
             [tech.thomas-sojka.plolj.utils :refer [translate tr-and]]))
 
 (def width 300)
 (def height 300)
-(def element (r/atom nil))
 
 (defn triangle [x1 y1 x2 y2 x3 y3]
   [:<>
@@ -38,25 +34,13 @@
       [sierpinski (/ length 2)]]]))
 
 (defn main []
-  [drawing-canvas {:width width :height height}
-   [:<>
-    [:svg {:xmlns "http://www.w3.org/2000/svg"
-           :width width
-           :height height
-           :viewBox (str "0 0 " width " " height)
-           :ref (fn [el] (when el (reset! element el)))}
-     [:rect {:x 0 :y 0 
-             :width (- width 1) :height (- height 1) :stroke "black"
-             :fill "transparent"
-             :stroke-width 1}]
-     [:g
-      {:transform (translate (/ width 2) (/ height 2))}
-      (let [length 240]
-        [:<>
-         
-         (equi-tri length)
-         (let [length (/ length 2)]
-           [:g {:transform (tr-and (translate 0 (/ length 2)) (rotate 180))}
-            [sierpinski length]])])]]
-    [download-button {:element @element}]
-    [plot-button {:element @element}]]])
+  [plot-canvas {:width width :height height}
+   [:g
+    {:transform (translate (/ width 2) (/ height 2))}
+    (let [length 240]
+      [:<>
+
+       (equi-tri length)
+       (let [length (/ length 2)]
+         [:g {:transform (tr-and (translate 0 (/ length 2)) (rotate 180))}
+          [sierpinski length]])])]])
