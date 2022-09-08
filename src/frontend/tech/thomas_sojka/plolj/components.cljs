d7fa478513a169bdfbb8fac92b1c7ca2e1370672
Thomas Sojka <thsojka@web.de>
2022-09-08 08:50:52 +0200

    [refactor] Use plot-canvas for distortion

--- src/frontend/tech/thomas_sojka/plolj/components.cljs
+++ src/frontend/tech/thomas_sojka/plolj/components.cljs
@@ -1,4 +1,5 @@
-(ns tech.thomas-sojka.plolj.components)
+(ns tech.thomas-sojka.plolj.components
+  (:require [reagent.core :as r]))
 
 (defn drawing-canvas [{:keys [width height on-drop style]} children]
   [:div#main.relative
@@ -28,15 +29,31 @@
           [:span {:style {:font-size 32 :color "#111827"}} "Download"]])
 
 (defn plot-button [{:keys [element]}]
-    [:button.shadow-2.pointer.bn.pv2.ph3.br3.grow.outline-0.mb1
+  [:button.shadow-2.pointer.bn.pv2.ph3.br3.grow.outline-0.ml1.mt5.mb1
    {:style {:bottom 0 :background-color "#F3F4F6" }
     :on-click (fn []
-                (prn "hi")
                 (let [s (new js/XMLSerializer)]
-                  (->  (js/fetch "http://localhost:8000/plot"
-                                 (clj->js
-                                  {:method "POST"
-                                   :headers {"content-type" "application/json"}
-                                   :body (.serializeToString s element)}))
-                       (.catch prn))))}
+                    (->  (js/fetch "http://localhost:8000/plot"
+                                   (clj->js
+                                    {:method "POST"
+                                     :headers {"content-type" "application/json"}
+                                     :body (.serializeToString s element)}))
+                         (.catch prn))))}
    [:span {:style {:font-size 32 :color "#111827"}} "Plot"]])
+
+(defn plot-canvas []
+  (let [svg-node! (r/atom nil)]
+    (fn [{:keys [width height]} children]
+      (prn svg-node!)
+      [:div
+       [drawing-canvas {:width width :height height}
+        [:svg
+         {:ref (fn [el] (prn el)(when el (reset! svg-node! el)))
+          :width width
+          :height height}
+         [:rect {:x 0 :y 0
+                 :width (- width 1) :height (- height 1) :stroke "black"
+                 :fill "transparent"
+                 :stroke-width 1}]
+         children]]
+       [plot-button {:element @svg-node!}]])))
--- src/frontend/tech/thomas_sojka/plolj/distortion.cljs
+++ src/frontend/tech/thomas_sojka/plolj/distortion.cljs
@@ -1,25 +1,11 @@
 (ns tech.thomas-sojka.plolj.distortion
   (:require
-   [tech.thomas-sojka.plolj.components :refer [drawing-canvas]]
+   [tech.thomas-sojka.plolj.components :refer [plot-canvas]]
    [clojure.string :as str]))
-
-(def svg (atom nil))
 
 (def width 300)
 (def height 300)
 (def step 14)
-
-(defn plot-button []
-  [:button.shadow-2.pointer.bn.pv2.ph3.br3.grow.outline-0.ml1.mt5.mb1
-   {:style {:bottom 0 :background-color "#F3F4F6" }
-    :on-click (fn []
-                (->  (js/fetch "http://localhost:8000/plot"
-                               (clj->js
-                                {:method "POST"
-                                 :headers {"content-type" "application/json"}
-                                 :body (.-outerHTML @svg)}))
-                     (.catch prn)))}
-   [:span {:style {:font-size 32 :color "#111827"}} "Plot"]])
 
 (defn pol->cart [{:keys [angle r]}]
   (let [x (+ (/ width 2) (* r (Math/cos angle)))
@@ -48,43 +34,32 @@
    {:f barrel-like-distortion :center {:angle (* Math/PI 1.75), :r 120}}])
 
 (defn main []
-  [:div
-   [drawing-canvas
-    {:width width
-     :height height}
-    [:svg
-     {:ref (fn [el] (when el (reset! svg el)))
-      :width width
-      :height height}
-     [:rect {:x 0 :y 0
-             :width (- width 1) :height (- height 1) :stroke "black"
-             :fill "transparent"
-             :stroke-width 1}]
-     (let [lines (->> (for [y (range step height step)
-                            x (range step width step)]
-                        [x y])
-                      (map-indexed vector)
-                      (keep (fn [[idx [x y]]]
-                              (cond
-                                (and (odd? idx) (odd? (Math/round (/ y step))))
-                                [x y]
-                                (and (odd? idx) (even? (Math/round (/ y step))))
-                                [x y])))
-                      (group-by second)
-                      (sort-by first)
-                      vals)]
-       (->> lines
-            (keep-indexed (fn [idx line] (when (> (count lines) (inc idx)) [line (nth lines (inc idx))])))
-            (map-indexed (fn [idx [line-a line-b]] (if (odd? idx) (interleave line-a line-b) (interleave line-b line-a))))
-            (map (fn [line] (map (fn [pos]
-                                  (str/join "," (->> distortions
-                                                     (reduce
-                                                      (fn [pos {:keys [f center]}]
-                                                        (distort pos f (pol->cart center)))
-                                                      pos))))
-                                line)))
-            (map-indexed (fn [idx points] [:polyline {:points (str/join " " points)
-                                                     :key idx
-                                                     :fill "none"
-                                                     :stroke "black"}]))))]]
-   [plot-button]])
+  [plot-canvas
+   {:width width :height height}
+   (let [lines (->> (for [y (range step height step)
+                          x (range step width step)]
+                      [x y])
+                    (map-indexed vector)
+                    (keep (fn [[idx [x y]]]
+                            (cond
+                              (and (odd? idx) (odd? (Math/round (/ y step))))
+                              [x y]
+                              (and (odd? idx) (even? (Math/round (/ y step))))
+                              [x y])))
+                    (group-by second)
+                    (sort-by first)
+                    vals)]
+     (->> lines
+          (keep-indexed (fn [idx line] (when (> (count lines) (inc idx)) [line (nth lines (inc idx))])))
+          (map-indexed (fn [idx [line-a line-b]] (if (odd? idx) (interleave line-a line-b) (interleave line-b line-a))))
+          (map (fn [line] (map (fn [pos]
+                                (str/join "," (->> distortions
+                                                   (reduce
+                                                    (fn [pos {:keys [f center]}]
+                                                      (distort pos f (pol->cart center)))
+                                                    pos))))
+                              line)))
+          (map-indexed (fn [idx points] [:polyline {:points (str/join " " points)
+                                                   :key idx
+                                                   :fill "none"
+                                                   :stroke "black"}]))))])
